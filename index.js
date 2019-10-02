0d99ecf1a58ea115d503050b79594d25d206d907
FelipeKatao <cataofelipe@gmail.com>
2019-10-02 00:00:21 -0300

    update

---
+++ index.js
@@ -0,0 +1,48 @@
+const { app, BrowserWindow } = require('electron')
+// Mantém a referência global do objeto da janela.
+// se você não fizer isso,
+// a janela será fechada automaticamente
+// quando o objeto JavaScript for coletado como lixo.
+let win
+
+function createWindow () {
+  // Criar uma janela de navegação.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    show:false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+  win.once("ready-to-show",()=>{win.show();})
+
+}
+
+// Este método será chamado quando o Electron tiver finalizado
+// a inicialização e está pronto para criar a janela browser.
+// Algumas APIs podem ser usadas somente depois que este evento ocorre.
+app.on('ready', createWindow)
+
+// Finaliza quando todas as janelas estiverem fechadas.
+app.on('window-all-closed', () => {
+  // No macOS é comum para aplicativos e sua barra de menu 
+  // permaneçam ativo até que o usuário explicitamente encerre com Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
+
+// Neste arquivo, você pode incluir o resto do seu aplicativo especifico do processo
+// principal. Você também pode colocar eles em arquivos separados e requeridos-as aqui.
