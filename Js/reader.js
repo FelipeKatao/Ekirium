f0153e65c53847fea3bc9fb0ecf12f099067097f
FelipeKatao <cataofelipe@gmail.com>
2019-10-06 01:21:53 -0300

    ajusts Reader.js returns

--- Js/reader.js
+++ Js/reader.js
@@ -2,13 +2,14 @@ function getReader(){
     
     var {PythonShell} = require('python-shell')
     var path = require('path')
-    var message="hello world";
+    var message="";
     var selectReader ={
         scriptPath: path.join(__dirname,'engine'),
         args:[message]
     }
     var reader = new PythonShell('reader.py',selectReader);
     reader.on('message',function(message){
+        if(message!=""){
         let removeEl = document.getElementById('welcome-page');
         let containerEl = removeEl.parentNode;
         containerEl.removeChild(removeEl);
@@ -16,6 +17,7 @@ function getReader(){
         let elementWork = document.getElementById('project-page-works');
         elementWork.style.visibility="visible";    
         ActiveMenu();
+        }
     })
 }
 
