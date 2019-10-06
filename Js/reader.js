2b30d876645d175abedbe78b2133e4c35b0470af
FelipeKatao <cataofelipe@gmail.com>
2019-10-06 01:02:01 -0300

    update Js functions

--- Js/reader.js
+++ Js/reader.js
@@ -12,10 +12,19 @@ function getReader(){
         let removeEl = document.getElementById('welcome-page');
         let containerEl = removeEl.parentNode;
         containerEl.removeChild(removeEl);
+        //Deixar visivel e habilitado partes dos menus//
         let elementWork = document.getElementById('project-page-works');
-        elementWork.style.visibility="visible";
-
+        elementWork.style.visibility="visible";    
+        ActiveMenu();
     })
+}
+
+function ActiveMenu(){
+    let elemnetItens = document.querySelectorAll('a');
+    for(var i=0;i<elemnetItens.length;i++){
+        elemnetItens[i].classList.remove('disabled');
+    }
+    
 }
 
 var  bt1 = document.getElementById('open-project');
